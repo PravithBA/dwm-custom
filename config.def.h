2bfc9617c234516c929094ce0eeaaae644ba0f83
Pravith <pravith10@gmail.com>
2023-04-20 12:51:11 +0530

    Feature: Changed color theme to rocepine.

--- config.def.h
+++ config.def.h
@@ -9,11 +9,11 @@ static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=20" };
 static const char dmenufont[]       = "monospace:size=20";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#31748f";
+static const char col_gray1[]       = "#191724";
+static const char col_gray2[]       = "#26233a";
+static const char col_gray3[]       = "#e0def4";
+static const char col_gray4[]       = "#ffc978";
+static const char col_cyan[]        = "#eb6f92";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char scratchpadname[] = "peaclock";
 static const char *scratchpadcmd[] = { "alacritty", "-e", scratchpadname, NULL };
