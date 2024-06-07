4be6373f7b94779f379a2a30a822c57e74f8faee
Pravith <pravith@getfinstack.in>
2024-06-07 13:28:20 +0530

    Feature: Postman is now in 4th tag.

--- config.def.h
+++ config.def.h
@@ -32,6 +32,7 @@ static const Rule rules[] = {
     {"Firefox", NULL, NULL, 1 << 1, 0, -1},
     {"firefox", NULL, NULL, 1 << 1, 0, -1},
     {"Slack", NULL, NULL, 1 << 2, 0, -1},
+    {"Postman", NULL, NULL, 1 << 3, 0, -1},
 };
 
 /* layout(s) */
